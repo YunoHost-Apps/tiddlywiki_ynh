d60f53873602262a15ca0da29f997168a333bd26
ericgaspar <junk.eg@free.fr>
2022-09-18 20:19:36 +0200

    Update updater.sh

--- .github/workflows/updater.sh
+++ .github/workflows/updater.sh
@@ -20,8 +20,8 @@
 current_version=$(cat manifest.json | jq -j '.version|split("~")[0]')
 repo=$(cat manifest.json | jq -j '.upstream.code|split("https://github.com/")[1]')
 # Some jq magic is needed, because the latest upstream release is not always the latest version (e.g. security patches for older versions)
-version=$(curl --silent "https://api.github.com/repos/$repo/releases" | jq -r '.[] | .tag_name' | sort -V | tail -1)
-assets=($(curl --silent "https://api.github.com/repos/$repo/releases" | jq -r '[ .[] | select(.tag_name=="'$version'").assets[].browser_download_url ] | join(" ") | @sh' | tr -d "'"))
+version=$(curl --silent "https://api.github.com/repos/$repo/tags" | jq -r '.[] | .tag_name' | sort -V | tail -1)
+assets=($(curl --silent "https://api.github.com/repos/$repo/tags" | jq -r '[ .[] | select(.tag_name=="'$version'").assets[].browser_download_url ] | join(" ") | @sh' | tr -d "'"))
 
 # if [[ ${version:0:1} == "v" || ${version:0:1} == "V" ]]; then
 #     version=${version:1}
